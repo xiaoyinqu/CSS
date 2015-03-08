e35c8c977eead60c165f6f8528f8ff1be52ef22f
Xiaoyin <xq002011@mymail.pomona.edu>
2015-03-08 15:58:01 -0700

    added

---
+++ trial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  trial
+//
+//  Created by QuXiaoyin on 3/7/15.
+//  Copyright (c) 2015 QuXiaoyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
