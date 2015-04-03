a08dd51537dadd8b4aa3dacd520abbdff3f80376
Xiaoyin <xq002011@mymail.pomona.edu>
2015-04-03 10:04:59 -0700

    ADDED NEW CHANGES

--- trial/ViewController.m
+++ trial/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
@@ -17,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)callPhone:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:9098257861"]];
+}
+- (IBAction)getDirections:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.google.com/maps/place/Claremont+Optometry/@34.107263,-117.725608,17z/data=!3m1!4b1!4m2!3m1!1s0x80c331c1833116db:0x5843b1ad57e1705d"]];
+}
+- (IBAction)callCSS:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:9096219900"]];
+}
+- (IBAction)callInnerValley:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:8002518191"]];
+}
+- (IBAction)callPomonaValley:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:9098659500"]];
 }
 
 - (void)didReceiveMemoryWarning {
